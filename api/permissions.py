67d613555f468e9572610cc1317985613040880d
wahab1 <wahabkazim10@gmail.com>
2025-03-14 14:28:45 +0500

    Remove extra lines in permission file

--- api/permissions.py
+++ api/permissions.py
@@ -1,31 +1,3 @@
-# from rest_framework import permissions
-
-
-# class Admin(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         return bool(request.user.is_superuser)
-
-
-# class StaffUser(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         if not request.user.is_staff:
-#             return False
-
-#         allowed_views = [
-#             'author_list', 'author_create', 'get_author', 'update_author', 'delete_author', 'book_list', 'book_create', 'get_book', 'update_book', 'delete_book', 
-#         ]
-#         return allowed_views
-
-
-# class RegularUser(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         if not request.user or not request.user.is_authenticated:
-#             return False
-#         if request.user.is_staff or request.user.is_superuser:
-#             return False
-#         allowed_views = ['borrow_book', 'return_book']
-#         return  allowed_views
-
 from rest_framework import permissions
 
 class Admin(permissions.BasePermission):
